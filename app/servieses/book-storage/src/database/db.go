f8b284182dc86824dda71c0d3c605815a4379ae8
osamikoyo <v.pylayev@mail.ru>
2024-09-05 20:35:26 +0500

    correct

--- app/servieses/book-storage/src/database/db.go
+++ app/servieses/book-storage/src/database/db.go
@@ -1,1 +1,20 @@
 package database
+
+import (
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+type Book struct{
+	Name string
+	Author string
+	Data string
+}
+
+func GetBooks() ([]Book, error){
+	var Books []Book
+	db, err := gorm.Open(sqlite.Open("storage/book.db"))
+	if err != nil {
+		return Books, err
+	}
+} 
