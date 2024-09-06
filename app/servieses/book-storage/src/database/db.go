b0baa65ca726f10fa3dfb6086053251b58dec18e
osamikoyo <v.pylayev@mail.ru>
2024-09-06 12:02:26 +0500

    register start

--- app/servieses/book-storage/src/database/db.go
+++ app/servieses/book-storage/src/database/db.go
@@ -1,20 +1,30 @@
 package database
 
 import (
+	"log/slog"
+	"os"
+
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
 type Book struct{
-	Name string
-	Author string
-	Data string
+	Name string `json:"name"`
+	Author string `json:"author"`
+	Data string `json:"data"`
 }
 
-func GetBooks() ([]Book, error){
-	var Books []Book
+func GetBooksFromDB() ([]Book, error){
+	loger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	var books []Book
 	db, err := gorm.Open(sqlite.Open("storage/book.db"))
 	if err != nil {
-		return Books, err
+		return books, err
 	}
+	result := db.Find(&books)
+	if result.Error != nil {
+		loger.Error(result.Error.Error())
+		return books, result.Error
+	}
+	return books, nil
 } 
--- app/servieses/register/src/database/db.go
+++ app/servieses/register/src/database/db.go
@@ -1,1 +1,21 @@
 package database
+
+import (
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+
+type User struct{
+	Username string `json:"username"`
+	Email string `json:"email"`
+	Password string `json:"password"`
+}
+
+func AddUserToDB(u User) error{
+	db, err := gorm.Open(sqlite.Open("storage/users.db"))
+	if err != nil {
+		return err
+	}
+	
+}
